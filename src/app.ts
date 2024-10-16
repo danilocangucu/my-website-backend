0b033d984c2738078db5f3f05783ad69e18decd6
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-16 21:10:19 +0200

    wip: enabled cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import ec2Routes from "./routes/ec2Routes";
+import cors from "cors";
 
 const app = express();
+
+// TODO - Add CORS policy
+app.use(cors());
 
 app.use(express.json());
 
