b43b1b7252da48ce7787cc65fdc0974a5ec5181e
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-18 15:39:18 +0200

    added delete route with terminateEC2Instance

--- src/routes/ec2Routes.ts
+++ src/routes/ec2Routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { startEC2Instance } from "../controllers/ec2Controller";
+import {
+  startEC2Instance,
+  terminateEC2Instance,
+} from "../controllers/ec2Controller";
 
 const router = Router();
 
 router.post("/", startEC2Instance);
+router.delete("/", terminateEC2Instance);
 
 export default router;
