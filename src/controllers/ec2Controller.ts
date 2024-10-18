e0e0898a454e19244d5022183055913b8cbc461e
Danilo Canguçu <daniloccangucu@gmail.com>
2024-10-18 15:39:02 +0200

    added terminateEC2Instance function

--- src/controllers/ec2Controller.ts
+++ src/controllers/ec2Controller.ts
@@ -2,7 +2,10 @@ import { Request, RequestHandler, Response } from "express";
 import { startEC2Validator } from "../validators/startEC2Validator";
 import { handleValidationError } from "../utils/errorHandler";
 import { ProjectName } from "../types/ec2Types";
-import { startEC2InstanceService } from "../services/ec2Service";
+import {
+  startEC2InstanceService,
+  terminateEC2InstanceService,
+} from "../services/ec2Service";
 
 export const startEC2Instance: RequestHandler = async (
   req: Request,
@@ -18,4 +21,20 @@ export const startEC2Instance: RequestHandler = async (
   const { projectName } = value as { projectName: ProjectName };
 
   await startEC2InstanceService(projectName, res);
+};
+
+export const terminateEC2Instance: RequestHandler = async (
+  req: Request,
+  res: Response
+) => {
+  const { error, value } = startEC2Validator.validate(req.body);
+
+  if (error) {
+    handleValidationError(res, error);
+    return;
+  }
+
+  const { projectName } = value as { projectName: ProjectName };
+
+  await terminateEC2InstanceService(projectName, res);
 };
