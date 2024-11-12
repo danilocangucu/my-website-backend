a6bdd6a27941fbe94dd58ea67b5f455f113db10a
Danilo Canguçu <daniloccangucu@gmail.com>
2024-11-12 19:15:23 +0100

    refactored hohohoController with its services

--- src/controllers/hohohoController.ts
+++ src/controllers/hohohoController.ts
@@ -2,8 +2,7 @@ import { Request, Response, RequestHandler } from "express";
 
 import { startApplicationValidator } from "../validators/hohohoValidators/startApplicationValidator";
 import { handleHohohoValidationError } from "../utils/errorHandler";
-import { generateVerificationCode } from "../utils/hohohoUtils";
-import { sendApplicationCodeEmail } from "../services/hohohoServices";
+import { startApplicationService } from "../services/hohohoServices";
 
 export const startApplication: RequestHandler = async (
   req: Request,
@@ -16,18 +15,5 @@ export const startApplication: RequestHandler = async (
     return;
   }
 
-  const verificationCode = generateVerificationCode();
-
-  try {
-    await sendApplicationCodeEmail(value.email, verificationCode);
-
-    res
-      .status(200)
-      .send({ message: "Email sent successfully with the application code." });
-  } catch (err) {
-    console.error("Error while sending email:", err);
-    res
-      .status(500)
-      .send({ message: "Failed to send email, please try again." });
-  }
+  await startApplicationService(value.email, res);
 };
--- src/services/hohohoServices.ts
+++ src/services/hohohoServices.ts
@@ -1,3 +1,6 @@
+import { Response } from "express";
+
+import { generateVerificationCode } from "../utils/hohohoUtils";
 import { sendHohohoEmailService } from "./emailsService";
 
 export const sendApplicationCodeEmail = async (
@@ -9,4 +12,27 @@ export const sendApplicationCodeEmail = async (
   const html = `<p>Here is your application code: <strong>${verificationCode}</strong></p>`;
 
   await sendHohohoEmailService(email, subject, text, html);
+};
+
+export const startApplicationService = async (
+  email: string,
+  res: Response
+): Promise<Response<any, Record<string, any>>> => {
+  const verificationCode = generateVerificationCode();
+
+  try {
+    await sendApplicationCodeEmail(email, verificationCode);
+
+    // Log the email and code to the database
+    // await logApplicationToDB(email, verificationCode);
+
+    return res
+      .status(200)
+      .send({ message: "Email sent successfully with the application code." });
+  } catch (error) {
+    console.error("Error in startApplicationService:", error);
+    return res
+      .status(500)
+      .send({ message: "Failed send your email with the application code." });
+  }
 };
